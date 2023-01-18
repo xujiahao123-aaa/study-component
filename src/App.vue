b50395204ad01facb35a81b36a1e7a7e0b304f08
xujiahao123-aaa <1148264186@qq.com>
2023-01-18 20:43:50 +0800

    component-demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <div class = "row">
+     <hm-button @click = "aa">按钮</hm-button>
+     <hm-button  type = "primary">按钮</hm-button>
+     <hm-button  type = "success">按钮</hm-button>
+     <hm-button  type = "info">按钮</hm-button>
+     <hm-button  type = "warning">按钮</hm-button>
+     <hm-button  type = "danger">按钮</hm-button>
+   </div>
+
+   <div class = "row">
+     <hm-button @click = "aa" plain>按钮</hm-button>
+     <hm-button plain  type = "primary">按钮</hm-button>
+     <hm-button plain  type = "success">按钮</hm-button>
+     <hm-button plain  type = "info">按钮</hm-button>
+     <hm-button plain  type = "warning">按钮</hm-button>
+     <hm-button plain  type = "danger">按钮</hm-button>
+   </div>
+
+   <div class = "row">
+     <hm-button @click = "aa" round>按钮</hm-button>
+     <hm-button round   type = "primary">按钮</hm-button>
+     <hm-button round   type = "success">按钮</hm-button>
+     <hm-button round   type = "info">按钮</hm-button>
+     <hm-button round   type = "warning">按钮</hm-button>
+     <hm-button round   type = "danger">按钮</hm-button>
+   </div>
+
+   <div class = "row">
+     <hm-button @click = "aa" circle>按</hm-button>
+     <hm-button circle  type = "primary">按</hm-button>
+     <hm-button circle  type = "success">按</hm-button>
+     <hm-button circle  type = "info">按</hm-button>
+     <hm-button circle  type = "warning">按</hm-button>
+     <hm-button circle  type = "danger">按</hm-button>
+   </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+   
+  methods: {
+    aa() {
+    console.log('1212')
+    }
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+ .row{
+  margin-top: 20px;
+    .hm-button{
+      margin-right:20px;
+    }
+ }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HmButton from './components/button.vue'
 
 Vue.config.productionTip = false
+
+Vue.component(HmButton.name, HmButton)
 
 new Vue({
   render: h => h(App),
